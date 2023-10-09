b2a7d842a1b6ff46982e0c56129caebca4c8f613
DeaconSt <ortiz-2394@hotmail.com>
2023-10-09 13:14:51 -0500

    Se agrega el proyecto

---
+++ src/app/app.component.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'Clot Strabe';
+
+  menu(){
+    var sec = document.getElementById('sec');
+    sec.classList.toggle('active');
+
+    var nav = document.getElementById('navigation');
+    nav.classList.toggle('active');
+  }
+}
